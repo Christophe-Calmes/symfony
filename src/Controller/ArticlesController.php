844e164bbcb93af7c542a5d64c9d0db80e171bcf
Calmes <christophe.calmes@laplateforme.io>
2022-09-22 17:57:27 +0200

    Navigation liste articles -> 1 artcile Ok

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -8,17 +8,32 @@ use Symfony\Component\Routing\Annotation\Route;
 
 // Utilisation de l'entity Articles
 use App\Repository\ArticlesRepository;
+// Utilisation de l'entity Comments
+use App\Repository\CommentsRepository;
 
 class ArticlesController extends AbstractController
 {
     #[Route('/articles', name: 'app_articles')]
-    public function index(ArticlesRepository $articlesRepository): Response
+    public function AllArticles(ArticlesRepository $articlesRepository): Response
     {
       // Récupérer les titres des articles
         $AllArticles = $articlesRepository->findAll();
 
         return $this->render('articles/index.html.twig', [
             'controller_name' => 'ArticlesController', 'Allarticles' => $AllArticles
+        ]);
+    }
+    #[Route('/OneArticles/{idArticle}', name: 'app_OneArticle')]
+    public function OneArticle(
+      ArticlesRepository $OneArticle, $idArticle): Response
+    {
+      // Récupérer un article
+        $detailArticle = $OneArticle->find($idArticle);
+        // CommentsRepository $AllComments, $idArticle
+        // Truc bizarre qui marche pas.
+        // $allCommentArticle = $AllComments->findBy(array('$idArticle' => $idArticle));
+        return $this->render('articles/OneArticle.html.twig', [
+            'controller_name' => 'ArticlesController', 'detailArticle' => $detailArticle
         ]);
     }
 }
