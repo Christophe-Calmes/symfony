64fc026198ea96b37e8a138dc1979d5e960aba62
Calmes <christophe.calmes@laplateforme.io>
2022-09-22 18:03:38 +0200

    Test autre solution.

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -8,8 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 // Utilisation de l'entity Articles
 use App\Repository\ArticlesRepository;
-// Utilisation de l'entity Comments
-use App\Repository\CommentsRepository;
+
 
 class ArticlesController extends AbstractController
 {
@@ -24,14 +23,10 @@ class ArticlesController extends AbstractController
         ]);
     }
     #[Route('/OneArticles/{idArticle}', name: 'app_OneArticle')]
-    public function OneArticle(
-      ArticlesRepository $OneArticle, $idArticle): Response
+    public function OneArticle(ArticlesRepository $OneArticle, $idArticle): Response
     {
       // Récupérer un article
         $detailArticle = $OneArticle->find($idArticle);
-        // CommentsRepository $AllComments, $idArticle
-        // Truc bizarre qui marche pas.
-        // $allCommentArticle = $AllComments->findBy(array('$idArticle' => $idArticle));
         return $this->render('articles/OneArticle.html.twig', [
             'controller_name' => 'ArticlesController', 'detailArticle' => $detailArticle
         ]);
